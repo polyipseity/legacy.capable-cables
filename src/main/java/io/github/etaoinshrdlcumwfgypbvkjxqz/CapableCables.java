6e586c49b13556c9b661ba941a90355f8099d7eb
William So <williamso26144225@gmail.com>
2019-12-25 01:37:53 +0800

    Add dependency 'codechicken:ForgeMultipart'

---
+++ src/main/java/io/github/etaoinshrdlcumwfgypbvkjxqz/CapableCables.java
@@ -0,0 +1,8 @@
+package io.github.etaoinshrdlcumwfgypbvkjxqz;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = CapableCables.MOD_ID, useMetadata = true, acceptedMinecraftVersions = "${minecraftVersionRange}", certificateFingerprint = "${certificateFingerprint}")
+public class CapableCables {
+    public static final String MOD_ID = "${modid}";
+}
