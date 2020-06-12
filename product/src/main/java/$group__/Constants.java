a92e2137282edc393c389fe8550c94aac1e77fc5
William So <williamso26144225@gmail.com>
2020-06-12 21:34:02 +0800

    Removed useless information from Constants.java.
    Signed-off-by: William So <williamso26144225@gmail.com>

--- product/src/main/java/$group__/Constants.java
+++ product/src/main/java/$group__/Constants.java
@@ -3,18 +3,6 @@ package $group__;
 public enum Constants {
 	/* MARK empty */;
 
-
-	/* SECTION static variables */
-
-	@SuppressWarnings({"SpellCheckingInspection", "RedundantSuppression"})
-	public static final String
-			NAME = "${name}",
-			VERSION = "${version}",
-			DEPENDENCIES = "${dependencies}",
-			ACCEPTED_MINECRAFT_VERSIONS = "${minecraftVersionRange}",
-			CERTIFICATE_FINGERPRINT = "${certificateFingerprint}",
-			UPDATE_JSON = "${updateJSON}";
-
 	@SuppressWarnings({"SpellCheckingInspection", "RedundantSuppression"})
 	public static final String
 			COFH_CORE_ID = "cofhcore", COFH_CORE_PACKAGE = "cofh",
