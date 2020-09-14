9e4a51ffabf2aeee927001d9615ec06a75999f58
William So <williamso26144225@gmail.com>
2020-09-14 23:25:17 +0800

    Template diff: 9c495ac664a74378ed8e0479bec791700de46735
    Signed-off-by: William So <williamso26144225@gmail.com>

--- subprojects/product/src/main/java/$group__/ConstantsProduct.java
+++ subprojects/product/src/main/java/$group__/ConstantsProduct.java
@@ -5,7 +5,7 @@ import $group__.utilities.compile.EnumBuildType;
 public enum ConstantsProduct {
 	;
 
-	public static final String MOD_ID = "${modId}";
+	public static final String MOD_ID = "${modID}";
 	public static final String NAME = "${name}";
 	public static final String BUILD_TYPE_STRING = "${buildType}";
 	public static final EnumBuildType BUILD_TYPE = EnumBuildType.valueOfSafe(BUILD_TYPE_STRING);
