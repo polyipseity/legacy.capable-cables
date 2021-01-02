add4b48079d7fe33d1dc19c5dd707187d206d6f3
William So <williamso26144225@gmail.com>
2021-01-02 15:37:08 +0800

    [ui] 🛠 Fix 'IUIThemeStack'
    [ui] 🛠 Fix 'IUIThemeStack'
    - Un-inherit from 'Deque' as it only allows for stack operations.  A
    bridging method to 'Deque' is added instead.
    - Update implementations.
    - Fix 'UIAbstractThemeStack#apply'.
    
    Signed-off-by: William So <williamso26144225@gmail.com>

--- subprojects/ui/src/main/java/io/github/etaoinshrdlcumwfgypbvkjxqz/capablecables/ui/def/theming/IUIThemeStack.java
+++ subprojects/ui/src/main/java/io/github/etaoinshrdlcumwfgypbvkjxqz/capablecables/ui/def/theming/IUIThemeStack.java
@@ -1,5 +1,6 @@
 package io.github.etaoinshrdlcumwfgypbvkjxqz.capablecables.ui.def.theming;
 
+import io.github.etaoinshrdlcumwfgypbvkjxqz.capablecables.annotations.Immutable;
 import io.github.etaoinshrdlcumwfgypbvkjxqz.capablecables.ui.def.mvvm.views.IUIViewCoordinator;
 import io.github.etaoinshrdlcumwfgypbvkjxqz.capablecables.ui.def.mvvm.views.rendering.IUIRendererContainer;
 
@@ -7,6 +8,13 @@ import java.util.Deque;
 import java.util.Iterator;
 
 public interface IUIThemeStack
-		extends Deque<IUITheme>, IUIViewCoordinator {
+		extends IUIViewCoordinator /* COMMENT do not extend 'Deque', bridge to 'Deque' provided by 'asDequeView' */ {
 	void apply(Iterator<? extends IUIRendererContainer<?>> rendererContainers);
+
+	void push(IUITheme theme);
+
+	IUITheme pop();
+
+	@SuppressWarnings("unused")
+	@Immutable Deque<? extends IUITheme> asDequeView();
 }
--- subprojects/ui/src/main/java/io/github/etaoinshrdlcumwfgypbvkjxqz/capablecables/ui/impl/theming/UIAbstractThemeStack.java
+++ subprojects/ui/src/main/java/io/github/etaoinshrdlcumwfgypbvkjxqz/capablecables/ui/impl/theming/UIAbstractThemeStack.java
@@ -1,11 +1,15 @@
 package io.github.etaoinshrdlcumwfgypbvkjxqz.capablecables.ui.impl.theming;
 
 import com.google.common.collect.ForwardingDeque;
+import com.google.common.collect.ImmutableList;
+import io.github.etaoinshrdlcumwfgypbvkjxqz.capablecables.annotations.Immutable;
 import io.github.etaoinshrdlcumwfgypbvkjxqz.capablecables.annotations.Nonnull;
 import io.github.etaoinshrdlcumwfgypbvkjxqz.capablecables.ui.def.mvvm.views.rendering.IUIRendererContainer;
 import io.github.etaoinshrdlcumwfgypbvkjxqz.capablecables.ui.def.theming.IUITheme;
 import io.github.etaoinshrdlcumwfgypbvkjxqz.capablecables.ui.def.theming.IUIThemeStack;
+import io.github.etaoinshrdlcumwfgypbvkjxqz.capablecables.utilities.collections.ListBackedDeque;
 
+import java.util.Deque;
 import java.util.Iterator;
 import java.util.function.Consumer;
 
@@ -35,7 +39,13 @@ public abstract class UIAbstractThemeStack
 
 	@Override
 	public void apply(Iterator<? extends IUIRendererContainer<?>> rendererContainers) {
+		Iterable<IUIRendererContainer<?>> rendererContainers1 = ImmutableList.copyOf(rendererContainers); // COMMENT will be iterated multiple times, copy iterator
 		descendingIterator() // COMMENT from tail to head, tail is the lowest theme, head is the highest theme
-				.forEachRemaining(theme -> theme.apply(rendererContainers));
+				.forEachRemaining(theme -> theme.apply(rendererContainers1.iterator()));
+	}
+
+	@Override
+	public @Immutable Deque<? extends IUITheme> asDequeView() {
+		return ListBackedDeque.ofImmutable(delegate());
 	}
 }
